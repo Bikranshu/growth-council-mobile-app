3a27f5926a236ed12731d36fd15c5adca24aea7f
Reshika Shrestha <shresthareshika@gmail.com>
2022-06-28 22:19:05 +0545

    navigation from notification in event details

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import { navigate } from './utils/navigationUtil';
-import { Platform } from 'react-native';
+import {navigate} from './utils/navigationUtil';
+import {Platform} from 'react-native';
 
 // import moment from 'moment';
 const PushNotificationsConfigs = {
   congigurations: () => {
     PushNotification.configure({
       onNotification: notification => {
+        const clicked = notification.userInteraction;
+
         if (Platform.OS == 'ios') {
           if (
             notification.foreground &&
@@ -15,31 +17,56 @@ const PushNotificationsConfigs = {
           ) {
             PushNotification.localNotification(notification);
           }
-          notification.finish(PushNotificationIOS.FetchResult.NoData);
+
+          if (clicked) {
+            // handle the navigation here
+            if (notification.data.type == 'chat') {
+              console.log(notification.data);
+              navigate('Chat', {
+                friendID: notification.data.friendID,
+                friendName: notification.data.friendName,
+                friendAvatar: notification.data.friendAvatar,
+                userID: notification.data.userID,
+                userName: notification.data.userName,
+                userAvatar: notification.data.userAvatar,
+              });
+            } else if (notification.data.type == 'event') {
+              navigate('EventDetail', {
+                id: notification.data.post_id,
+                image: require('../src/assets/img/appBG.png'),
+                title: 'Growth Community',
+              });
+            }
+          }
+
+            notification.finish(PushNotificationIOS.FetchResult.NoData);
         } else {
           if (notification.foreground) {
             PushNotification.localNotification(notification);
           }
         }
 
-        const clicked = notification.userInteraction;
         if (clicked) {
-            // handle the navigation here
-            if(notification.data.type == 'chat'){
-                console.log(notification.data);
-                navigate('Chat', {
-                    friendID: notification.data.friendID,
-                    friendName: notification.data.friendName,
-                    friendAvatar: notification.data.friendAvatar,
-                    userID: notification.data.userID,
-                    userName: notification.data.userName,
-                    userAvatar: notification.data.userAvatar,
-                  })
-            } else if (notification.data.type == "event"){
-                navigate("EventDetail", {id: notification.data.post_id});
-            }
+          // handle the navigation here
+          if (notification.data.type == 'chat') {
+            console.log(notification.data);
+            navigate('Chat', {
+              friendID: notification.data.friendID,
+              friendName: notification.data.friendName,
+              friendAvatar: notification.data.friendAvatar,
+              userID: notification.data.userID,
+              userName: notification.data.userName,
+              userAvatar: notification.data.userAvatar,
+            });
+          } else if (notification.data.type == 'event') {
+            navigate('EventDetail', {
+              id: notification.data.post_id,
+              image: require('../src/assets/img/appBG.png'),
+              title: 'Growth Community',
+            });
           }
-          notification.finish(PushNotificationIOS.FetchResult.NoData);
+        }
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
       onAction: notification => {
         console.log('NOTIFICATION:', notification);
