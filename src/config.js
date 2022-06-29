54964e053fc139ab0fa456ce6aa37e7c1b650e77
Saqib Afridi <saqibafridi@Saqibs-MacBook-Pro.local>
2022-06-29 14:23:16 +0500

    fixed ios crash on notification

--- src/config.js
+++ src/config.js
@@ -3,40 +3,44 @@ import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import { navigate } from './utils/navigationUtil';
 import { Platform } from 'react-native';
 
+const isIOS = Platform.OS == 'ios';
+
 // import moment from 'moment';
 const PushNotificationsConfigs = {
   congigurations: () => {
     PushNotification.configure({
       onNotification: notification => {
-        if (Platform.OS == 'ios') {
-          if (
-            notification.foreground &&
-            (notification.userInteraction || notification.remote)
-          ) {
-            PushNotification.localNotification(notification);
-          }
-          notification.finish(PushNotificationIOS.FetchResult.NoData);
-        } else {
-          if (notification.foreground) {
-            PushNotification.localNotification(notification);
-          }
+
+        if(notification.foreground){
+          PushNotification.localNotificationSchedule({
+            ...notification,
+            userInfo: notification?.data,
+            date: new Date(Date.now() + 10 * 1000)
+          });
         }
 
         const clicked = notification.userInteraction;
+
         if (clicked) {
-            // handle the navigation here
-            if(notification.data.type == 'chat'){
-                console.log(notification.data);
-                navigate('Chat', {
-                    friendID: notification.data.friendID,
-                    friendName: notification.data.friendName,
-                    friendAvatar: notification.data.friendAvatar,
-                    userID: notification.data.userID,
-                    userName: notification.data.userName,
-                    userAvatar: notification.data.userAvatar,
-                  })
-            } else if (notification.data.type == "event"){
-                navigate("EventDetail", {id: notification.data.post_id});
+            try {
+              // handle the navigation here
+              const data = notification?.data || notification?.userInfo;
+
+            if(data?.type == 'chat'){
+              console.log(notification.data);
+              navigate('Chat', {
+                  friendID: data?.friendID,
+                  friendName: data?.friendName,
+                  friendAvatar: data?.friendAvatar,
+                  userID: data?.userID,
+                  userName: data?.userName,
+                  userAvatar: data?.userAvatar,
+                })
+          } else if (data?.type == "event"){
+              navigate("EventDetail", {id: data?.post_id});
+          }
+            } catch(error){
+              console.log(error);
             }
           }
           notification.finish(PushNotificationIOS.FetchResult.NoData);
