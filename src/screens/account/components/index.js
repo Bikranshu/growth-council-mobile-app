504a19b523e4d98617140f4177726d02a60d3fcd
Reshika Shrestha <shresthareshika@gmail.com>
2022-11-22 14:38:35 +0545

    countdown in profile

--- src/screens/account/components/index.js
+++ src/screens/account/components/index.js
@@ -13,7 +13,8 @@ import {
 import Font from 'react-native-vector-icons/FontAwesome5';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import ButtonToggleGroup from 'react-native-button-toggle-group';
-import {BubblesLoader} from 'react-native-indicator';
+import CountDown from 'react-native-countdown-component';
+import moment from 'moment';
 import analytics from '@react-native-firebase/analytics';
 import {CommonStyles, Colors, Typography} from '../../../theme';
 import {PRIMARY_BACKGROUND_COLOR} from '../../../theme/colors';
@@ -37,11 +38,32 @@ const Profile = props => {
   const win = Dimensions.get('window');
 
   const [value, setValue] = useState('About me');
+  const [totalDuration, setTotalDuration] = useState(0);
   const isFocused = useIsFocused();
 
   useEffect(() => {
     fetchProfileByIdentifier();
   }, [isFocused]);
+
+  useEffect(() => {
+    //We are showing the coundown timer for a given expiry date-time
+    //If you are making a quize type app then you need to make a simple timer
+    //which can be done by using the simple like given below
+    //that.setState({ totalDuration: 30 }); //which is 30 sec
+    var date = new Date();
+    //Getting the current date-time with required formate and UTC
+    var expirydate = '2022-12-23 04:00:45'; //You can set your own date-time
+    //Let suppose we have to show the countdown for above date-time
+    var diffr = moment.duration(moment(expirydate).diff(moment(date)));
+    //difference of the expiry date-time given and current date-time
+    var hours = parseInt(diffr.asHours());
+    var minutes = parseInt(diffr.minutes());
+    var seconds = parseInt(diffr.seconds());
+    var d = hours * 60 * 60 + minutes * 60 + seconds;
+    //converting in seconds
+    setTotalDuration(d);
+    //Settign up the duration of countdown in seconds to re-render
+  }, [profile]);
 
   return (
     <View style={{flex: 1}}>
@@ -125,13 +147,37 @@ const Profile = props => {
               <View style={styles.header}>
                 <Text style={styles.headingText1}>{profile?.user_login}</Text>
                 <Text style={{color: '#222B45'}}>
-					{profile?.user_meta?.title === undefined
-                      ? profile?.user_meta?.Title[0]
-                      : profile?.user_meta?.title[0]}
+                  {profile?.user_meta?.title === undefined
+                    ? profile?.user_meta?.Title[0]
+                    : profile?.user_meta?.title[0]}
                   {/* {profile?.user_meta?.title} */}
                 </Text>
               </View>
             </View>
+          </View>
+          <View style={{marginVertical: 20}}>
+            <CountDown
+              until={totalDuration}
+              //style of countdown
+              digitStyle={{
+                backgroundColor: '#FFF',
+                borderWidth: 2,
+                borderColor: '#1CC625',
+              }}
+              digitTxtStyle={{color: '#1CC625'}}
+              timeLabelStyle={{color: 'red', fontWeight: 'bold'}}
+              separatorStyle={{color: '#1CC625', marginBottom: 20}}
+              //duration of countdown in seconds
+              timetoShow={('H', 'M', 'S')}
+              timeLabels={{d: 'Days', h: 'Hour', m: 'Min', s: 'Sec'}}
+              //formate to show
+              onFinish={() => alert('finished')}
+              //on Finish call
+              onPress={() => alert('hello')}
+              //on Press call
+              size={20}
+              showSeparator
+            />
           </View>
 
           <View style={styles.container}>
